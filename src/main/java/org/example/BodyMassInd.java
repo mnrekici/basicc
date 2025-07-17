c8138cf96e579481fa19d13e174b065209a35aff
mnrekici <munireekici7@gmail.com>
2025-07-18 00:27:57 +0300

    Tüm dosyalar eklendi

--- src/main/java/org/example/BodyMassInd.java
+++ src/main/java/org/example/BodyMassInd.java
@@ -1,4 +1,20 @@
 package org.example;
 
+
+
+import java.util.Scanner;
+
 public class BodyMassInd {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen boyunuzu metre cinsinde giriniz:");
+        double height = scanner.nextDouble();
+
+        System.out.println("Lütfen kilonuzu giriniz:");
+        double weight = scanner.nextDouble();
+
+        double body_mass_index = weight / (height * height);
+        System.out.println("Vücut kitle indeksiniz: " + body_mass_index);
+    }
+} double müni= 21
